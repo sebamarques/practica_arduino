05aad1e4a6526be23ca24d8908ba014731b4ab78
sebamarques <sebamarquezblanco@gmail.com>
2024-04-29 20:11:50 -0300

    ajedrez_v0.1

--- ajedrez/ajedrez.ino
+++ ajedrez/ajedrez.ino
@@ -1,40 +1,31 @@
-int jugador1 = 600;
-int jugador2 = 600;
-int boton1 = 4;
-int boton2 = 5;
+int tiempo_jugador1 = 600;
+int tiempo_jugador2 = 600;
+int boton1 = 3;
+int interruptor = 4;
 int valor1;
-int valor2;
-int led1=6;
-int led2=7;
-void setup(){
-Serial.begin(9600);
-  pinMode(boton1,INPUT);
-  pinMode(boton2,INPUT);
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(boton1, INPUT);
+  pinMode(interruptor, INPUT);
 }
 
-
-void loop(){
-  valor1 = analogRead(boton1);
-  valor2 = analogRead(boton2);
-  while(jugador1 > 0|| jugador2 > 0){
-  if (valor1 == LOW){
-    jugador1-=1;
-    valor2 = LOW;
-    digitalWrite(led1,HIGH);
-    digitalWrite(led2,LOW);
-    }
-   if (valor2 == LOW){
-    jugador2-=1;
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,HIGH);
-    }
-   if(jugador1 ==0){
-    Serial.println("El jugador 1 perdio");
-    break;}
-   if(jugador2 ==0){
-    Serial.println("El jugador 2 perdio");
-    break;}
-   }
-  }
+void loop() {
+  valor1 = digitalRead(boton1);
+  //boton para activar el reloj de ajedrez
+  while(valor1 == LOW){
+    int valor2 = digitalRead(interruptor);
+    while(valor2==LOW){
+      valor2 = digitalRead(interruptor);
+      tiempo_jugador1--;
+      Serial.println("Tiempo jugador1:");
+      Serial.println(tiempo_jugador1);
+      delay(1000);
+      if(valor2 == HIGH){
+        break;
+        }
+      }
+      
+  }  
+}
